a310609022413fc193a7d91102b16e0b7f5fc42c
Soumalya Mondal <iamsoumalya007@hotmail.com>
2026-06-03 13:24:13 +0530

    Database Table Create Micro-Service Created

--- supportscripts/dbtablecreate.py
+++ supportscripts/dbtablecreate.py
@@ -1,18 +1,18 @@
-def db_table_create(database_file_path):
+# define "db_table_create" function to create SQLite database and table
+def db_table_create(database_file_path: str) -> dict[str, str]:
     # Importing Python Module:S1
     try:
         import sqlite3
     except Exception as error:
-        return {'status': 'error', 'step': '1', 'file_name': 'DB-Table-Create', 'message': str(error)}
+        return {'status': 'ERROR', 'step': '1', 'file_name': 'DB-Table-Create', 'message': str(error)}
 
     # Create Database Connection And Cursor:S2
     try:
         # create database connection and cursor
         database_connection = sqlite3.connect(str(database_file_path))
         database_cursor = database_connection.cursor()
-        print(f'SUCCESS - Database Connection And Cursor Created Successfully')
     except Exception as error:
-        return {'status': 'error', 'step': '2', 'file_name': 'DB-Table-Create', 'message': str(error)}
+        return {'status': 'ERROR', 'step': '2', 'file_name': 'DB-Table-Create', 'message': str(error)}
 
     # Create SQLite Database And Table:S3
     try:
@@ -20,7 +20,8 @@ def db_table_create(database_file_path):
         create_table_query = """
         CREATE TABLE IF NOT EXISTS interview_qa_table (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            question_text TEXT NOT NULL DEFAULT 'N/A',
+            actual_question_text TEXT NOT NULL DEFAULT 'N/A',
+            final_question_text TEXT NOT NULL DEFAULT 'N/A',
             answer_text TEXT NOT NULL DEFAULT 'N/A',
             input_token INTEGER NOT NULL DEFAULT 0,
             output_token INTEGER NOT NULL DEFAULT 0,
@@ -28,8 +29,12 @@ def db_table_create(database_file_path):
             model_name TEXT DEFAULT 'N/A'
         )
         """
+        # execute the create table query and commit the changes
         database_cursor.execute(create_table_query)
         database_connection.commit()
-        return {'status': 'success', 'step': '3', 'file_name': 'DB-Table-Create', 'message': 'SQLite Database And Table Created Successfully'}
+        # close the database connection and return success message
+        database_connection.close()
+        return {'status': 'SUCCESS', 'step': '3', 'file_name': 'DB-Table-Create', 'message': 'SQLite Database And Table Created Successfully'}
     except Exception as error:
-        return {'status': 'error', 'step': '3', 'file_name': 'DB-Table-Create', 'message': str(error)}
+        database_connection.close()
+        return {'status': 'ERROR', 'step': '3', 'file_name': 'DB-Table-Create', 'message': str(error)}
